3b6b3f4737b7550ec48f2cb353ceb90788f3779a
Learningitnow <70935742+Learningitnow@users.noreply.github.com>
2020-10-16 12:44:02 -0400

    move folders

--- server.js
+++ server.js
@@ -1,38 +1,45 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const fs = require('fs')
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000
+const saveFile = './.tableList.json' // save file, it's a hidden file
 
 // will share any static html files with the browser
-app.use( express.static('html') );
+app.use( express.static('html') )
 // accept incoming POST requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Data =======================================================
-let tableList = [ { name: 'test table', phone: '999-999-9999'} ]
+let tableList = fs.existsSync(saveFile) ?
+    JSON.parse( fs.readFileSync(saveFile) ) : []
+// let tableList = [ ]
 
 // Routes (Endpoints) =========================================
-app.get('/api/tables/:tableId', function(req, res) {
-    const tableId = req.params.tableId;
-    // get the table info
-    const tableData = {};
-    res.send( tableData );
-});
-
 app.get('/api/tables', function(req, res) {
-    res.send( tableList );
-});
+    res.send( tableList )
+})
+
+app.get( '/api/tables/clear', function( req, res ){
+    // clear all the table entries
+    console.log( 'about to clear all table entries!')
+    tableList = []
+    fs.writeFileSync( saveFile, JSON.stringify( tableList ) )
+    res.send( { message: 'Just nuked all table entries' } )
+})
 
 app.post('/api/tables/reserve', function(req, res) {
-    console.log( '[reservation] req.body: ', req.body );
+    const newTableData = req.body
+    console.log( `[reservation] tableList(${tableList.length} entries), adding newTableData: \n`, newTableData )
+    tableList.push( newTableData )
+    // save to a file, as a string like localStorage
+    fs.writeFileSync( saveFile, JSON.stringify( tableList ) )
 
-    const newTableData = req.body;
-    res.send( { message: `Saved: ${newTableData.name}` } );
+    res.send( { message: `Reserved for *${newTableData.name}*` } )
 });
 
 // Listener ==================================================
 app.listen(PORT, function() {
-    console.log('Serving hot-tables on PORT ' + PORT);
-});
+    console.log('Serving hot-tables on PORT ' + PORT)
+})
